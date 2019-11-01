a1aa2adf408fdc5b0774c4fbbd4412306e29dbc6
Ald <45129401+Ion388901@users.noreply.github.com>
2019-10-31 22:31:00 -0600

    Implementación parcial de AI enemiga

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -10,11 +10,12 @@ public class EnemyController : MonoBehaviour
     Ray enemyRay;
     RaycastHit playerDetected;
     public Color rayColor;
+    public bool follow;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Enemy = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
@@ -23,7 +24,12 @@ public class EnemyController : MonoBehaviour
         enemyRay = new Ray(transform.position, transform.forward * 10);
         Debug.DrawRay(transform.position, transform.forward * 10, rayColor);
 
-        if(Physics.Raycast(enemyRay, 10))
+        if(Physics.Raycast(transform.position, transform.forward, out playerDetected, 10.0f))
+        {
+            follow = true;
+        }
+
+        if(follow == true)
         {
             Enemy.SetDestination(Player.transform.position);
         }
