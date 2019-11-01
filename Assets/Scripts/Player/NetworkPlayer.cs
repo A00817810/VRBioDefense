b632072578e66b23e4410c192c7f85367eca34de
A00817810 <A00817810@itesm.mx>
2019-11-01 10:26:42 -0600

    Created Network Player
    Apenas empiezo el script

---
+++ Assets/Scripts/Player/NetworkPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class ToggleEvent : UnityEvent<bool>{ }
+
+public class NetworkPlayer : NetworkBehaviour
+{
+    [SerializeField]
+    private ToggleEvent onToggleShared;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
