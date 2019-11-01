e8792bda02473ee1da5912cbe3bc6cda9d58df7b
InJoong <injoong01@hotmail.com>
2019-11-01 11:17:53 -0600

    Regrese

---
+++ Assets/Scripts/Attack/Attack.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    private int damage;
+    private GameObject attacker;
+
+    public int Damage { get => damage; set => damage = value; }
+    public GameObject Attacker { get => attacker; set => attacker = value; }
+}
---
+++ Assets/Scripts/AttackerDamage.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class AttackerDamage : MonoBehaviour
+{
+    private HealthMediator healthMediator;
+    private LastAttackerMediator lastAttackerMediator;
+
+    void Awake()
+    {
+        if ((healthMediator = GetComponent<HealthMediator>()) == null)
+        {
+            healthMediator = this.gameObject.AddComponent<HealthMediator>();
+        }
+        if ((lastAttackerMediator = GetComponent<LastAttackerMediator>()) == null)
+        {
+            lastAttackerMediator = this.gameObject.AddComponent<LastAttackerMediator>();
+        }
+    }
+
+    void Rest()
+    {
+        if ((healthMediator = GetComponent<HealthMediator>()) == null)
+        {
+            healthMediator = this.gameObject.AddComponent<HealthMediator>();
+        }
+        if ((lastAttackerMediator = GetComponent<LastAttackerMediator>()) == null)
+        {
+            lastAttackerMediator = this.gameObject.AddComponent<LastAttackerMediator>();
+        }
+    }
+
+    public void CalculateDamage(object sender, BodyPhysicsEventArgs e)
+    {
+        Attack attack = e.target.GetComponent<Attack>();
+        if (attack != null)
+        {
+            lastAttackerMediator.SetLasAttacker(e.target);
+            healthMediator.DecreaseCurrentHealth(attack.Damage);
+        }
+    }
+}
